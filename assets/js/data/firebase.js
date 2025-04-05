d9b4a61fbe507c43c8340105039aa17ad364f021
Miriã Amaral Custódio Santos <miriaamaralcustodio@gmail.com>
2025-04-05 14:17:13 -0300

    Add files via upload

---
+++ assets/js/data/firebase.js
@@ -0,0 +1,6 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js'
+
+import firebaseConfig from './config'
+
+const app = initializeApp(firebaseConfig)
+export default app
